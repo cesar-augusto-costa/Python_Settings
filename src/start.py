5d517e0b2cb524e58b25d3bbcf4a577547f35ecb
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-19 11:51:08 -0300

    config: setting project configs

---
+++ src/start.py
@@ -0,0 +1,23 @@
+""" Module Docstring """
+
+
+def soma(num_1: int, num_2: int) -> int:
+    """Somando dois números
+    :param - num_1: primeiro número da soma
+    :param - num_2: segundo número da soma
+    :return - Soma entre dois números
+    """
+    soma_dois_numeros = num_1 + num_2
+    return soma_dois_numeros
+
+
+SOMA_DOIS_NUMEROS = soma(2, 4)
+
+
+def minha_funcao(
+    argumento_muito_grande_1,
+    argumento_muito_grande_2,
+    argumento_muito_grande_3,
+    argumento_muito_grande_4,
+):
+    pass
